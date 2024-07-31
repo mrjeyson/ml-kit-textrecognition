5c95dd6f20c89bf9a9d6537550a9bd06d1183eef
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-07-31 11:18:48 +0500

    ...

--- app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionWithCameraProvider.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionWithCameraProvider.kt
@@ -233,7 +233,6 @@ fun OverlayFrame(
 fun LicensePlateScannerScreen1() {
 
     var recognizedText by remember { mutableStateOf("") }
-
     val roi = Rect(
         left = 0.05f, top = 0.5f, right = 0.95f, bottom = 0.6f
     )
