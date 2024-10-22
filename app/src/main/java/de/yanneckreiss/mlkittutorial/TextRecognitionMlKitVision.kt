08af8b3b3153ba67da83ce4dd98152f17160eb71
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-10-22 12:25:42 +0500

    ...

--- app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionMlKitVision.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionMlKitVision.kt
@@ -38,7 +38,6 @@ fun LicensePlateScannerScreen2(modifier: Modifier = Modifier) {
 
 
 
-
     var recognizedText by remember { mutableStateOf("") }
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
