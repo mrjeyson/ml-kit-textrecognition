56a807da3054ce9c842e683364e0f76ac9560bda
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-10-25 08:34:32 +0500

    ...

--- app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionMlKitVision.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/TextRecognitionMlKitVision.kt
@@ -46,6 +46,7 @@ fun LicensePlateScannerScreen2(modifier: Modifier = Modifier) {
 
 
 
+
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
     ) { granted ->
