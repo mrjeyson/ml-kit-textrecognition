bbf1f681dcaa81152b900d12a932fd3bfaded655
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-11-27 08:43:57 +0500

    ...

--- app/src/main/java/de/yanneckreiss/mlkittutorial/MainActivity.kt
+++ app/src/main/java/de/yanneckreiss/mlkittutorial/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import de.yanneckreiss.mlkittutorial.ui.theme.JetpackComposeMLKitTutorialTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
